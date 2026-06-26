fc27bad11e56fb3bf11e4210bf788d49df3498ef
baur <alexba.baur@protonmail.com>
2026-06-26 13:37:25 +0200

    set to correct device rank

--- train.py
+++ train.py
@@ -455,6 +455,7 @@ def train(rank, args, cfg):
     batch_size = cfg["training_cfg"]["batch_size"] // cfg["env_setting"]["num_gpus"]
     if num_gpus >= 1:
         initialize_process_group(cfg, rank)
+        torch.cuda.set_device(rank)
         device = torch.device("cuda:{:d}".format(rank))
     else:
         raise RuntimeError("Mamba needs GPU acceleration")
