a186bc40f07ce0a5dc4b0a63182c139691e24da7
baur <alexba.baur@proton.me>
2026-07-14 16:51:24 +0200

    added train script for 4-real-mode selective LRU

---
+++ tasks/selective_lru/train_selective_lru_L_8rm.sh
@@ -0,0 +1,22 @@
+#!/bin/bash -l
+#
+#SBATCH --cpus-per-task=32
+#SBATCH --ntasks=1
+#SBATCH --partition=a40
+#SBATCH --job-name=selective_lru_L_4rm
+#SBATCH --output=jobs/%x-%j.out
+#SBATCH --error=jobs/%x-%j.err
+#SBATCH --time=23:59:00
+#SBATCH --requeue
+#SBATCH --signal=B:SIGUSR1@120
+#SBATCH --open-mode=append
+#SBATCH --export=NONE
+#SBATCH --gres=gpu:a40:2
+
+source tasks/_env.sh
+source tasks/_requeue.sh
+
+run_with_requeue srun --unbuffered --ntasks=1 python -u train.py \
+  --config recipes/selective/SelectiveLRU_L_4rm.yaml \
+  --exp_folder /home/atuin/f102ac/f102ac13/SEMambaBackbones/ \
+  --exp_name SelectiveLRU_L_4rm
